447f5db270e43d0800329b9af0b739872ee974af
michaelocez <m.huangnzc@gmail.com>
2026-04-22 14:17:43 +1200

    remove whitespace and create frequency table for uc car colours

--- carpark_vehicles.R
+++ carpark_vehicles.R
@@ -1,2 +1,12 @@
 library(tidyverse)
 carpark.all <- read.csv('carpark_data.csv')
+
+
+# Remove white space
+carpark.all <- carpark.all |>
+  mutate(Colour = str_trim(Colour))
+
+# Frequency table for UC vehicles
+carpark.colour <- carpark.all |>
+  count(Colour, name = "UC_Vehicles") |>
+  arrange(Colour)
