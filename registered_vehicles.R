d82dd878ec8f110764b1ca14e26b0c95d9649f89
michaelocez <m.huangnzc@gmail.com>
2026-04-22 14:42:58 +1200

    remove total count, rename and select colour and count for NZ vehicles

--- registered_vehicles.R
+++ registered_vehicles.R
@@ -2,4 +2,6 @@ library(tidyverse)
 registered.all <- read_csv('Registered_vehicles_2025.csv')
 registered.colour <- registered.all %>% 
                       filter(`Grouping variable` == 'Basic colour', 
-                             `Vehicle type` == 'Passenger Car/Van') 
+                             `Vehicle type` == 'Passenger Car/Van',
+                             Category != 'Total') |>
+  select(Colour = Category, NZ_Vehicles = Value) # Rename category to colour, value to NZ_Vehicles and only keep those two columns
